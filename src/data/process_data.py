3bfecae8ab8bac8cbf203713416c6caaf6c385ed
Victor <victortolsagerolesen1@gmail.com>
2023-01-29 13:52:22 +0100

    finish split data and unit tests for process_data. Fix type hints, add sort fn as an argument

--- src/data/process_data.py
+++ src/data/process_data.py
@@ -1,10 +1,7 @@
 import torch
+from collections.abc import Callable, Iterable
 import numpy as np
-
-
-def process_cifar10() -> dict[str : torch.Tensor]:
-    # load train data
-    pass
+from typing import *
 
 
 class DataSplitter:
@@ -20,41 +17,63 @@ class DataSplitter:
         self.n_shards = shards_per_client * n_clients
         self.n_samples = len(self.dataset)
 
-    def get_shard_indices(self):
+    def get_shard_start_indices(self) -> np.ndarray:
+        """generates the start indices of each shard
+
+        Returns:
+            np.ndarray: array of indices
+        """
         return np.linspace(0, self.n_samples, num=self.n_shards, endpoint=False).astype(
             int
         )
 
+    def split_data(self) -> list[torch.utils.data.Dataset]:
+        """uses the shard start indices to get all the shards
+        from the data and combine them into the client datasets
 
-def sort_torch_dataset(dataset: torch.utils.data.Dataset) -> torch.utils.data.Dataset:
+        Returns:
+            list[torch.utils.data.Dataset]: client datasets
+        """
+        shard_indices = self.get_shard_start_indices()
+
+        # randomly select shards_per_client shards
+        shard_assignments = np.random.choice(
+            range(len(shard_indices)),
+            size=(self.n_clients, self.shards_per_client),
+            replace=False,
+        )
+
+        # append extra element to shard_indices to prevent going out of bounds when splitting data
+        shard_indices = np.append(shard_indices, self.n_samples)
+
+        # from the shard indices assigned to each client, get the
+        # shards as pytorch subsets
+        client_datasets = []
+        for split in shard_assignments:
+            temp_datasets = []
+            for shard_idx in split:
+                start_idx = shard_indices[shard_idx]
+                end_idx = shard_indices[shard_idx + 1]
+                data_subset = torch.utils.data.Subset(
+                    self.dataset, range(start_idx, end_idx)
+                )
+                temp_datasets.append(data_subset)
+
+            client_dataset = torch.utils.data.ConcatDataset(temp_datasets)
+            client_datasets.append(client_dataset)
+
+        return client_datasets
+
+
+def sort_torch_dataset(
+    dataset: torch.utils.data.Dataset, sort_fn: Callable[[], Any]
+) -> torch.utils.data.Dataset:
     # sort the data by label
     sort_fn = lambda x: x[1]
     sorted_ds = sorted(dataset, key=sort_fn)
     return sorted_ds
 
 
-def split_data_to_clients(
-    dataset: torch.utils.data.Dataset, n_clients: int, shards_per_client: int = 2
-):
-    if not isinstance(dataset, torch.utils.data.Dataset):
-        raise ValueError("Expected a torch dataset")
-
-    n_samples = len(dataset)
-    total_shards = n_clients * shards_per_client
-    shard_size = np.floor(n_samples / total_shards)
-
-    # sort dataset
-
-    # if more shards are requested than there are data point, raise error
-    if shard_size == 0:
-        raise ValueError(
-            "The number of shards needed exceed the number of samples in the dataset provided"
-        )
-
-    # create a sampler
-    sampler = torch.utils.data.SequentialSampler(dataset)
-
-    #
-    batch_sample = torch.utils.data.BatchSampler(
-        sampler, batch_size=shard_size, drop_last=False
-    )
+def cifar10_sort_fn(batch: tuple[torch.Tensor]):
+    # return the label
+    return batch[1]
--- tests/data/test_process_data.py
+++ tests/data/test_process_data.py
@@ -1,18 +1,49 @@
 import torch
+import torchvision
 import numpy as np
-from src.data.process_data import DataSplitter
+from src.data.process_data import DataSplitter, sort_torch_dataset, cifar10_sort_fn
 import pytest
 import os
 
+save_path = "data/raw"
+transforms = torchvision.transforms.Compose([torchvision.transforms.ToTensor()])
+dataset = torchvision.datasets.CIFAR10(
+    root=save_path, train=False, transform=transforms, download=False
+)
+dataset = torch.utils.data.Subset(dataset, range(100))
 
-@pytest.mark.skipif(not os.path.exists("data/raw/test.pt"))
+
 class TestDatasplitter:
-    dataset = torch.load("data/raw/test.pt")
-    dataset = torch.utils.data.Subset(dataset, 100)
-    n_clients = 10
-    shards_per_client = 1
-    datasplitter = DataSplitter(dataset, n_clients, shards_per_client)
+    n_clients = [5, 10]
+    shards_per_client = [2, 1]
+    datasplitters = [
+        DataSplitter(dataset, clients, shards)
+        for clients, shards in zip(n_clients, shards_per_client)
+    ]
+    dataset = dataset
 
     def test_get_shard_indices(self):
-        shard_indices = self.datasplitter.get_shard_indices()
-        assert shard_indices == np.array([0, 10, 20, 30, 40, 50, 60, 70, 80, 90])
+        for datasplitter in self.datasplitters:
+            shard_indices = datasplitter.get_shard_start_indices()
+            assert np.all(
+                shard_indices == np.array([0, 10, 20, 30, 40, 50, 60, 70, 80, 90])
+            )
+
+    def test_split_data(self):
+        for i, datasplitter in enumerate(self.datasplitters):
+            splits = datasplitter.split_data()
+            assert len(splits) == self.n_clients[i]
+            len_split0 = len(splits[0])
+
+            for split in splits:
+                assert isinstance(split, torch.utils.data.Dataset)
+                assert len(split) >= len_split0 and len(split) < (len_split0 + 2)
+
+
+def test_sort_torch_dataset():
+    sort_fn = cifar10_sort_fn
+    sorted_ds = sort_torch_dataset(dataset, sort_fn)
+    prev = sorted_ds[0]
+    # test that the data is strictly increasing which implies that it's sorted
+    for i in range(1, len(sorted_ds)):
+        assert sorted_ds[i][1] >= sorted_ds[i - 1][1]
