1af5dc2b8e96cdc7b7e095cd0b80a973523bb22f
Victor <victortolsagerolesen1@gmail.com>
2023-02-01 15:31:24 +0100

    add seed everything

--- src/utils.py
+++ src/utils.py
@@ -1,3 +1,8 @@
+import os
+import random
+
+import numpy as np
+import torch
 import yaml
 from yaml.loader import SafeLoader
 
@@ -31,3 +36,16 @@ def load_config(name: str) -> dict:
     config = load_yaml("configs/" + config_path)
 
     return config
+
+
+def seed_everything(seed_value):
+    random.seed(seed_value)
+    np.random.seed(seed_value)
+    torch.manual_seed(seed_value)
+    os.environ["PYTHONHASHSEED"] = str(seed_value)
+
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed(seed_value)
+        torch.cuda.manual_seed_all(seed_value)
+        torch.backends.cudnn.deterministic = True
+        torch.backends.cudnn.benchmark = True
