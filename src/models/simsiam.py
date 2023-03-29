c5e05786ff8d248dd612abd2ad4111f99caa1add
Victor <victortolsagerolesen1@gmail.com>
2023-03-29 17:44:27 +0200

    add projector and predictor

--- src/models/simsiam.py
+++ src/models/simsiam.py
@@ -284,3 +284,56 @@ class SimSiam(nn.Module):
         p2 = self.predictor(z2)
 
         return {"z1": z1, "z2": z2, "p1": p1, "p2": p2}
+
+
+class projection_MLP(nn.Module):
+    def __init__(self, in_dim, out_dim, num_layers=2):
+        super().__init__()
+        hidden_dim = out_dim
+        self.num_layers = num_layers
+
+        self.layer1 = nn.Sequential(
+            nn.Linear(in_dim, hidden_dim),
+            nn.BatchNorm1d(hidden_dim),
+            nn.ReLU(inplace=True),
+        )
+
+        self.layer2 = nn.Sequential(
+            nn.Linear(hidden_dim, hidden_dim),
+            nn.BatchNorm1d(hidden_dim),
+            nn.ReLU(inplace=True),
+        )
+        self.layer3 = nn.Sequential(
+            nn.Linear(hidden_dim, out_dim),
+            nn.BatchNorm1d(out_dim, affine=False),  # Page:5, Paragraph:2
+        )
+
+    def forward(self, x):
+        if self.num_layers == 2:
+            x = self.layer1(x)
+            x = self.layer3(x)
+        elif self.num_layers == 3:
+            x = self.layer1(x)
+            x = self.layer2(x)
+            x = self.layer3(x)
+        return x
+
+
+class prediction_MLP(nn.Module):
+    def __init__(self, in_dim=2048):
+        super().__init__()
+        out_dim = in_dim
+        hidden_dim = int(out_dim / 4)
+
+        self.layer1 = nn.Sequential(
+            nn.Linear(in_dim, hidden_dim),
+            nn.BatchNorm1d(hidden_dim),
+            nn.ReLU(inplace=True),
+        )
+        self.layer2 = nn.Linear(hidden_dim, out_dim)
+
+    def forward(self, x):
+        x = self.layer1(x)
+        x = self.layer2(x)
+
+        return x
