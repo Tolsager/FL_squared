a4037aabed19e4c64f8f763956dd3dcdaeebf4e0
Victor <victortolsagerolesen1@gmail.com>
2023-03-29 17:36:13 +0200

    add pytorch implementation

--- src/models/simsiam.py
+++ src/models/simsiam.py
@@ -1,21 +1,16 @@
 import math
-from typing import Tuple
+from typing import Optional, Tuple
 
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import torchmetrics
+import tqdm
 from pytorch_lightning import Callback, LightningModule
+from torch import nn
 
+from src.metrics import KNN
 from src.models.metrics import KNN
-
-
-class SimSiam:
-    pass
-
-
-class SimSiamModel(SimSiam):
-    def __init__(self, **kwargs):
-        pass
 
 
 class SimpNetEncoder(nn.Module):
@@ -109,129 +104,183 @@ class SimpNetEncoder(nn.Module):
         return encoding
 
 
-class OurSimSiam(LightningModule):
+# def asymmetric_loss(p, z):
+#     z = z.detach()  # stop gradient
+
+#     return -torch.nn.functional.cosine_similarity(p, z, dim=-1).mean()
+
+
+# def criterion(model_outputs: dict):
+#     loss1 = asymmetric_loss(model_outputs["p1"], model_outputs["z2"])
+#     loss2 = asymmetric_loss(model_outputs["p2"], model_outputs["z1"])
+#     return 0.5 * loss1 + 0.5 * loss2
+class SimSiamLoss(nn.Module):
+    def __init__(self, version="simplified"):
+        super().__init__()
+        self.ver = version
+
+    def asymmetric_loss(self, p, z):
+        if self.ver == "original":
+            z = z.detach()  # stop gradient
+
+            p = nn.functional.normalize(p, dim=1)
+            z = nn.functional.normalize(z, dim=1)
+
+            return -(p * z).sum(dim=1).mean()
+
+        elif self.ver == "simplified":
+            z = z.detach()  # stop gradient
+            return -nn.functional.cosine_similarity(p, z, dim=-1).mean()
+
+    def forward(self, z1, z2, p1, p2):
+
+        loss1 = self.asymmetric_loss(p1, z2)
+        loss2 = self.asymmetric_loss(p2, z1)
+
+        return 0.5 * loss1 + 0.5 * loss2
+
+
+def adjust_learning_rate(optimizer, epoch, args):
+    """Decay the learning rate based on schedule"""
+    lr = args["learning_rate"]
+    # cosine lr schedule
+    lr *= 0.5 * (1.0 + math.cos(math.pi * epoch / args["epochs"]))
+
+    for param_group in optimizer.param_groups:
+        param_group["lr"] = lr
+
+
+class Trainer:
     def __init__(
         self,
-        backbone: torch.nn.Module,
-        projector: torch.nn.Module,
-        predictor: torch.nn.Module,
-        learning_rate: float = 0.001,
-        weight_decay: float = 0.01,
-        max_epochs: int = 100,
+        train_dataloader: torch.utils.data.DataLoader,
+        val_dataloader: Optional[torch.utils.data.DataLoader],
+        model: torch.nn.Module,
+        epochs: int = 10,
+        learning_rate: float = 0.06,
+        weight_decay: float = 5e-4,
+        device: str = "cuda",
+        validation_interval: int = 5,
     ):
-        super().__init__()
-        # self.feature_dim = feature_dim
-        self.backbone = backbone
-        self.projector = projector
-        self.predictor = predictor
-        self.criterion = torch.nn.CosineSimilarity(dim=-1)
+        self.train_dataloader = train_dataloader
+        self.val_dataloader = val_dataloader
+        self.model = model.to(device)
+        self.device = device
+        self.epochs = epochs
         self.learning_rate = learning_rate
         self.weight_decay = weight_decay
-        self.max_epochs = max_epochs
-
-    def forward(self, x1, x2):
-        z1 = self.backbone(x1)
-        z1 = self.projector(z1)
-        z2 = self.backbone(x2)
-        z2 = self.projector(z2)
-
-        prediction1 = self.predictor(z1)
-        prediction2 = self.predictor(z2)
-
-        return prediction1, prediction2, z1.detach(), z2.detach()
-
-    def training_step(self, batch: Tuple[torch.Tensor, torch.Tensor], batch_idx: int):
-        im1, im2, image, label = batch
-        prediction1, prediction2, z1, z2 = self(im1, im2)
-        loss = -0.5 * (
-            self.criterion(prediction1, z2).mean()
-            + self.criterion(prediction2, z1).mean()
+        self.optimizer = torch.optim.SGD(
+            model.parameters(), learning_rate, momentum=0.9, weight_decay=weight_decay
         )
-        self.log("train_loss", loss, on_epoch=True)
-        return loss
+        self.avg_train_loss = torchmetrics.MeanMetric()
+        self.criterion = SimSiamLoss()
+        self.validation_interval = validation_interval
 
-    def configure_optimizers(self):
-        optimizer = torch.optim.SGD(
-            self.parameters(),
-            lr=self.learning_rate,
-            weight_decay=self.weight_decay,
-            momentum=0.9,
-        )
-        scheduler = ConsineLRScheduler(optimizer, self.max_epochs)
-        return [optimizer], [{"scheduler": scheduler, "interval": "epoch"}]
+    def train_epoch(self) -> None:
+        self.model.train()
+        # for aug1, aug2, _, _ in tqdm.tqdm(self.train_dataloader):
+        for i, (images, _) in enumerate(self.train_dataloader):
 
+            # aug1 = aug1.to(self.device)
+            # aug2 = aug2.to(self.device)
+            images[0] = images[0].cuda()
+            images[1] = images[1].cuda()
+            model_outputs = self.model(im_aug1=images[0], im_aug2=images[1])
+            loss = self.criterion(
+                model_outputs["z1"],
+                model_outputs["z2"],
+                model_outputs["p1"],
+                model_outputs["p2"],
+            )
 
-class KNNCallback(Callback):
-    def __init__(
-        self,
-        val_dataloader: torch.utils.data.DataLoader,
-        knn_k: int = 200,
-        top_k: list[int] = [1],
-        n_classes: int = 10,
-    ):
-        self.val_dataloader = val_dataloader
-        self.knn_k = knn_k
-        self.top_k = top_k
-        self.n_classes = n_classes
+            # optimization
+            self.optimizer.zero_grad()
+            loss.backward()
+            self.optimizer.step()
 
-    def on_train_epoch_end(self, trainer, pl_module):
-        train_dataloader = trainer.train_dataloader
+            loss = loss.detach().cpu().item()
+            self.avg_train_loss.update(loss)
+
+    def train(self) -> None:
+        for epoch in tqdm.trange(self.epochs):
+            adjust_learning_rate(
+                self.optimizer,
+                epoch=epoch,
+                args={"learning_rate": self.learning_rate, "epochs": self.epochs},
+            )
+            self.avg_train_loss.reset()
+            self.train_epoch()
+            avg_train_loss = self.avg_train_loss.compute()
+            print(f"Epoch: {epoch}")
+            print(f"Average train loss: {avg_train_loss}")
+            if epoch != 0 and (epoch % self.validation_interval) == 0:
+                val_acc = self.validation()
+                print(f"Validation accuracy: {val_acc}")
+
+    def validation(self) -> float:
+        self.model.eval()
         train_features = []
         train_labels = []
         val_features = []
         val_labels = []
 
-        pl_module.eval()
         with torch.no_grad():
-            for batch in train_dataloader:
-                _, _, im, label = batch
-                im = im.cuda()
-                train_features.append(pl_module.backbone(im).cpu())
+            for batch in self.train_dataloader:
+                images, label = batch
+                img = images[-1]
+                img = img.cuda()
+                train_features.append(self.model.backbone(img).cpu())
                 train_labels.append(label.cpu())
 
             train_features = torch.concat(train_features, dim=0).numpy()
             train_labels = torch.concat(train_labels, dim=0).numpy()
 
             for batch in self.val_dataloader:
-                im, label = batch
-                im = im.cuda()
-                val_features.append(pl_module.backbone(im).cpu())
+                img, label = batch
+                img = img.cuda()
+                val_features.append(self.model.backbone(img).cpu())
                 val_labels.append(label.cpu())
 
             val_features = torch.concat(val_features, dim=0).numpy()
             val_labels = torch.concat(val_labels, dim=0).numpy()
 
-            knn = KNN(n_classes=self.n_classes, top_k=self.top_k, knn_k=self.knn_k)
+            knn = KNN(n_classes=10, top_k=[1], knn_k=200)
             val_acc = knn.knn_acc(
                 val_features, val_labels, train_features, train_labels
             )
 
-        pl_module.log("val_acc", val_acc)
+        return list(val_acc.values())[0]
 
 
-def get_simsiam_predictor(embedding_dim: int = 432, hidden_dim: int = 200):
-    predictor = torch.nn.Sequential(
-        torch.nn.Linear(embedding_dim, hidden_dim),
-        torch.nn.BatchNorm1d(hidden_dim),
-        torch.nn.ReLU(inplace=True),
-        torch.nn.Linear(hidden_dim, embedding_dim),
-    )
-    return predictor
+class SimSiam(nn.Module):
+    def __init__(self):
+        super(SimSiam, self).__init__()
+        self.backbone = SimSiam.get_backbone("resnet18")
+        out_dim = self.backbone.fc.weight.shape[1]
+        self.backbone.fc = nn.Identity()
 
+        self.projector = projection_MLP(out_dim, 2048, 2)
 
-class ConsineLRScheduler(torch.optim.lr_scheduler._LRScheduler):
-    def __init__(self, optimizer, max_epochs: int, last_epoch: int = -1):
-        self.epoch = 1
-        self.max_epochs = max_epochs
-        self.lr_factor = 0.5 * (1.0 + math.cos(math.pi / self.max_epochs))
-        super(ConsineLRScheduler, self).__init__(
-            optimizer, last_epoch=last_epoch, verbose=False
-        )
+        self.encoder = nn.Sequential(self.backbone, self.projector)
 
-    def get_lr(self):
-        if self.last_epoch == 0:
-            return [group["lr"] for group in self.optimizer.param_groups]
-        factor = 0.5 * (1.0 + math.cos(math.pi * self.epoch / self.max_epochs))
-        lrs = [group["lr"] * factor for group in self.optimizer.param_groups]
-        self.epoch += 1
-        return lrs
+        self.predictor = prediction_MLP(2048)
+
+    @staticmethod
+    def get_backbone(backbone_name):
+        return {
+            "resnet18": resnet.ResNet18(),
+            "resnet34": resnet.ResNet34(),
+            "resnet50": resnet.ResNet50(),
+            "resnet101": resnet.ResNet101(),
+            "resnet152": resnet.ResNet152(),
+        }[backbone_name]
+
+    def forward(self, im_aug1, im_aug2):
+
+        z1 = self.encoder(im_aug1)
+        z2 = self.encoder(im_aug2)
+
+        p1 = self.predictor(z1)
+        p2 = self.predictor(z2)
+
+        return {"z1": z1, "z2": z2, "p1": p1, "p2": p2}
--- src/models/train_model.py
+++ src/models/train_model.py
@@ -21,205 +21,55 @@ def cli():
     pass
 
 
-@click.command()
-@click.option("--config-file", type=str, default="debug_train")
-def train_fl(config_file: str):
-    config = utils.load_config(config_file)
-    tags = ["debug"]
-    wandb.init(config=config, project="rep-in-fed", entity="pydqn", tags=tags)
-    config = wandb.config
-    config.server.n_clients
-    server = model.Server(**config["server"])
-    server.train()
+import torch
+import torchvision
+
+from src import data, engine, model
 
 
-@click.command(name="baseline")
-@click.option("--learning-rate", type=float, default=0.001)
-@click.option("--batch-size", type=int, default=16)
-@click.option("--seed", type=int, default=0)
-@click.option("--epochs", type=int, default=5)
-def train_bl(learning_rate: float, batch_size: int, seed: int, epochs: int):
-    utils.seed_everything(seed)
+def train():
+    # get datasets
+    # train_ds = data.get_Vcifar10_dataset(train=True)
+    val_ds = data.get_cifar10_dataset(train=False)
 
-    logger = WandbLogger(
-        project="rep-in-fed",
-        entity="pydqn",
-        notes="simpnet baseline with less augmentations,\
-        reduced dropout probabilities, groupnorm after all layers",
+    # cifar10_train_transforms = torchvision.transforms.Compose(
+    #     data.cifar10_augmentations
+    # )
+    # cifar10_val_transforms = torchvision.transforms.Compose(
+    #     data.cifar10_standard_transforms
+    # )
+    # train_ds = data.SimSiamDataset(train_ds, cifar10_train_transforms)
+    # train_ds = data.SimSiamDataset(train_ds, data.train_transforms)
+    val_ds = data.AugmentedDataset(val_ds, data.base_transforms)
+
+    # create dataloaders
+    train_ds = torchvision.datasets.CIFAR10(
+        "data/raw",
+        train=True,
+        download=True,
+        transform=data.TwoCropsTransform(data.train_transforms),
     )
-    transforms = process_data.get_cifar10_transforms()
-
-    # baseline = model.SimpNet(embedding_size=10, learning_rate=learning_rate)
-    baseline = model.ClientCNN(learning_rate=learning_rate)
-    train, test = model.make_dataset.load_dataset()
-    train, val = process_data.train_val_split(test, 0.2)
-    train = process_data.AugmentedDataset(train, transforms)
-    trainloader_bl = torch.utils.data.DataLoader(train, batch_size=batch_size)
-    valloader_bl = torch.utils.data.DataLoader(val, batch_size=batch_size)
-    trainer = Trainer(accelerator="gpu", gpus=1, max_epochs=epochs, logger=logger)
-    trainer.fit(baseline, trainloader_bl, valloader_bl)
-
-
-@click.command(name="simsiam")
-@click.option("--learning-rate", type=float, default=0.001)
-@click.option("--batch-size", type=int, default=32)
-@click.option("--seed", type=int, default=0)
-@click.option("--epochs", type=int, default=2)
-@click.option("--embedding-size", type=int, default=2048)
-@click.option("--debug", is_flag=True)
-@click.option("--backbone", default="resnet")
-@click.option("--linear-lr", is_flag=True)
-@click.option("--trials", type=int, default=1)
-def train_simsiam(
-    learning_rate: float,
-    batch_size: int,
-    seed: int,
-    epochs: int,
-    embedding_size: int,
-    debug: bool,
-    backbone: str,
-    linear_lr: bool,
-    trials: int,
-):
-    backbones = {"resnet", "simpnet"}
-    if backbone not in backbones:
-        raise ValueError(
-            f"Backbone {backbone} is not supported\n available \
-                backbones are: f{backbones}"
-        )
-    utils.seed_everything(seed)
-
-    if linear_lr:
-        learning_rate = 0.03 * batch_size / 256
-
-    train = torchvision.datasets.CIFAR10(root="data/raw", train=True)
-    val = torchvision.datasets.CIFAR10(
-        root="data/raw",
-        train=False,
-        transform=torchvision.transforms.Compose(
-            process_data.cifar10_standard_transforms
-        ),
+    train_dl = torch.utils.data.DataLoader(
+        train_ds, batch_size=512, num_workers=8, pin_memory=True, shuffle=True
+    )
+    val_dl = torch.utils.data.DataLoader(
+        val_ds, batch_size=512, num_workers=8, pin_memory=True
     )
 
-    transforms = process_data.get_simsiam_transforms(img_size=32)
-    train = process_data.SimSiamDataset(train, transforms)
+    simsiam = model.SimSiam()
 
-    trainloader_bl = torch.utils.data.DataLoader(train, batch_size=batch_size)
-    valloader_bl = torch.utils.data.DataLoader(val, batch_size=batch_size)
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    print(f"Training on: {device}")
 
-    for trial in range(trials):
-        seed = seed + trial
-        utils.seed_everything(seed)
-
-        if not debug:
-            tags = ["simsiam", "high learning rate"]
-            notes = input("Please provide a description of the experiment:\n")
-            logger = WandbLogger(
-                project="rep-in-fed", entity="pydqn", tags=tags, notes=notes
-            )
-
-        if backbone == "resnet":
-            backbone_model = resnet.ResNet18()
-            projector = torch.nn.Sequential(
-                torch.nn.Linear(512, embedding_size),
-                torch.nn.BatchNorm1d(embedding_size),
-                torch.nn.ReLU(inplace=True),
-                torch.nn.Linear(embedding_size, embedding_size),
-                torch.nn.BatchNorm1d(embedding_size, affine=False),
-            )
-
-        elif backbone == "simpnet":
-            backbone_model = model.SimpNet(
-                embedding_size=embedding_size, learning_rate=learning_rate
-            )
-
-        predictor = simsiam.get_simsiam_predictor(
-            embedding_dim=embedding_size, hidden_dim=512
-        )
-        simsiam_model = simsiam.OurSimSiam(
-            backbone=backbone_model,
-            projector=projector,
-            predictor=predictor,
-            learning_rate=learning_rate,
-            weight_decay=0.0005,
-            max_epochs=epochs,
-        )
-        if debug:
-            trainer = Trainer(limit_train_batches=2, limit_val_batches=2)
-        else:
-            knn_callback = simsiam.KNNCallback(
-                val_dataloader=valloader_bl, knn_k=200, top_k=[1], n_classes=10
-            )
-            trainer = (
-                Trainer(
-                    accelerator="gpu",
-                    gpus=1,
-                    max_epochs=epochs,
-                    logger=logger,
-                    callbacks=[knn_callback],
-                    log_every_n_steps=10,
-                )
-                if GPU
-                else Trainer(max_epochs=epochs, logger=logger)
-            )
-
-        trainer.fit(
-            simsiam_model,
-            trainloader_bl,
-        )
-        if not debug:
-            wandb.finish()
-
-
-@click.command(name="imagenet")
-@click.option("--learning-rate", type=float, default=0.001)
-@click.option("--batch-size", type=int, default=32)
-@click.option("--seed", type=int, default=0)
-@click.option("--epochs", type=int, default=2)
-@click.option("--embedding-size", type=int, default=1000)
-@click.option("--arch", type=str, default="simpnet")
-def train_imagenet(
-    learning_rate: float,
-    batch_size: int,
-    seed: int,
-    epochs: int,
-    embedding_size: int,
-    arch: str,
-):
-    utils.seed_everything(seed)
-
-    tags = ["supervised_learning", f"{arch}", "imagenet"]
-
-    logger = WandbLogger(project="rep-in-fed", entity="pydqn", tags=tags)
-    if arch == "simpnet":
-        simpnet_model = model.SimpNet(embedding_size, learning_rate=learning_rate)
-
-    train = torchvision.datasets.ImageFolder(
-        "data/raw/imagenet/train",
-        transform=torchvision.transforms.Compose(
-            process_data.imagenet_standard_transforms
-        ),
+    simsiam.to(device)
+    # trainer = engine.Trainer(
+    #     train_dl, val_dl, simsiam, epochs=100, learning_rate=0.06, device=device
+    # )
+    trainer = engine.Trainer(
+        train_dl, val_dl, simsiam, epochs=300, learning_rate=0.06, device=device
     )
-    train = process_data.shuffle_dataset(train)
-    val = torchvision.datasets.ImageFolder(
-        "data/raw/imagenet/val",
-        transform=torchvision.transforms.Compose(
-            process_data.imagenet_standard_transforms
-        ),
-    )
-    trainloader = torch.utils.data.DataLoader(train, batch_size=batch_size)
-    valloader = torch.utils.data.DataLoader(val, batch_size=batch_size)
+    trainer.train()
 
-    trainer = Trainer(
-        accelerator="gpu", gpus=1, max_epochs=epochs, logger=logger
-    )  # Not feasible to use a non GPU machine to train
-    trainer.fit(simpnet_model, trainloader, valloader)
-
-
-cli.add_command(train_fl)
-cli.add_command(train_bl)
-cli.add_command(train_simsiam)
-cli.add_command(train_imagenet)
 
 if __name__ == "__main__":
     cli()
