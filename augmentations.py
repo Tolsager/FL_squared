94a638acc0ce686f7b91a0a20e0eb06ffc8606f1
Victor <victortolsagerolesen1@gmail.com>
2023-05-25 16:19:32 +0200

    more augs

--- augmentations.py
+++ augmentations.py
@@ -9,7 +9,7 @@ cifar = torchvision.datasets.CIFAR10(root="data/raw")
 image = cifar[-57][0]
 
 
-def plot_augmentations(image: Image, transforms: list):
+def plot_random_augmentations(image: Image, transforms: list):
     fig, ax = plt.subplots(len(transforms) + 1, 5)
 
     ax[0, 0].set_title("Original Image", fontsize=8)
@@ -19,7 +19,12 @@ def plot_augmentations(image: Image, transforms: list):
         ax[i, 0].set_xticklabels([])
         ax[i, 0].set_yticklabels([])
         ax[i, 0].tick_params(axis="both", which="both", length=0)
-        ax[i, 0].set_ylabel(type(transform).__name__ if type(transform).__name__ != "RandomApply" else "ColorJitter", fontsize=5)
+        ax[i, 0].set_ylabel(
+            type(transform).__name__
+            if type(transform).__name__ != "RandomApply"
+            else "ColorJitter",
+            fontsize=5,
+        )
         for j in range(1, 5):
             ax[i, j].axis("off")
             ax[i, j].imshow(transform(image))
@@ -33,30 +38,31 @@ def plot_augmentations(image: Image, transforms: list):
         ax[-1, j].imshow(torchvision.transforms.Compose(transforms)(image))
     ax[-1, 0].set_ylabel("Combined", fontsize=5)
 
-    plt.subplots_adjust(left=0.04,
-                        bottom=0.03,
-                        right=0.750,
-                        top=0.85,
-                        wspace=0.05,
-                        hspace=0.2)
+    plt.subplots_adjust(
+        left=0.04, bottom=0.03, right=0.750, top=0.85, wspace=0.05, hspace=0.2
+    )
 
     # Use to adjust the plot interactively
-    # plt.subplot_tool()
-
+    plt.subplot_tool()
 
     plt.savefig(f"1.png", bbox_inches="tight", dpi=300)
     plt.show()
 
+
 crop_transform = torchvision.transforms.RandomCrop(32, 4)
 rotation_transform = torchvision.transforms.RandomRotation(10)
-flip_transform = torchvision.transforms.RandomHorizontalFlip(0.5)
-colorjitter = torchvision.transforms.ColorJitter(brightness=0.4, contrast=0.4, saturation=0.4, hue=0.1)
+# flip_transform = torchvision.transforms.RandomHorizontalFlip(0.5)
+colorjitter = torchvision.transforms.ColorJitter(
+    brightness=0.4, contrast=0.4, saturation=0.4, hue=0.1
+)
 resized_crop = torchvision.transforms.RandomResizedCrop(32, scale=(0.2, 1.0))
-grayscale = torchvision.transforms.RandomGrayscale(p=0.4)
+# grayscale = torchvision.transforms.RandomGrayscale(p=0.4)
 
-simple_transforms = [crop_transform, rotation_transform, flip_transform]
-simsiam_transforms = [resized_crop, flip_transform, torchvision.transforms.RandomApply([colorjitter], p=0.8), grayscale]
+simple_transforms = [crop_transform, rotation_transform]
+simsiam_transforms = [
+    resized_crop,
+    torchvision.transforms.RandomApply([colorjitter], p=0.8),
+]
 
 
-plot_augmentations(image, simple_transforms)
-
+plot_random_augmentations(image, simple_transforms)
